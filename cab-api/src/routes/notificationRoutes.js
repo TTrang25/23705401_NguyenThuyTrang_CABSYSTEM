83a8df3887afbcf5fc542f722e99b887eb223b5c
TTrang25 <146370206+TTrang25@users.noreply.github.com>
2026-09-13 16:42:17 +0700

    Delete cab-api directory

--- cab-api/src/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticate } = require('../middleware/auth');
-const notificationController = require('../controllers/notificationController');
-
-router.use(authenticate);
-router.get('/', notificationController.getMyNotifications);
-
-module.exports = router;
